2745b663ef40313a207eaf961d18558dd30ec962
Gayashan Wagachchi <gayashan4lk@gmail.com>
2022-03-18 23:37:20 +0530

    Basic types, arrays, tuples

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,26 @@
 "use strict";
+// Basic Types
 let id = 5;
-console.log("ID :", id);
+let company = "Virtusa";
+let isPublished = true;
+let x = "Hello";
+let age;
+age = 30;
+let idStore = [1, 2, 3, 4, 5];
+idStore.push(6);
+console.log(idStore);
+let mixArr = [1, "one", true, { data: "some data" }];
+mixArr.push("two");
+mixArr.push(2);
+mixArr.push({ name: "Leonado" });
+console.log(mixArr);
+// Tuple
+let person = [1, "Davinci", true];
+// Tuple Array
+let employee;
+employee = [
+    [1, "Fyodor Dostoevsky"],
+    [2, "Maxim Gorky"],
+    [3, "Dan Brown"]
+];
+console.log(employee);
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,29 @@
+// Basic Types
 let id: number = 5;
+let company: string = "Virtusa";
+let isPublished: boolean = true;
+let x: any = "Hello";
 
-console.log("ID :" , id);
+let age: number
+age = 30;
+
+let idStore: number[] = [1, 2, 3, 4, 5];
+idStore.push(6);
+console.log(idStore);
+
+let mixArr: any[] = [1, "one", true, {data : "some data"}];
+mixArr.push("two");
+mixArr.push(2);
+mixArr.push({name : "Leonado"});
+console.log(mixArr);
+
+// Tuple
+let person: [number, string, boolean] = [1, "Davinci", true];
+// Tuple Array
+let employee: [number, string][];
+employee = [
+  [1, "Fyodor Dostoevsky"],
+  [2, "Maxim Gorky"],
+  [3, "Dan Brown"]
+]
+console.log(employee);
